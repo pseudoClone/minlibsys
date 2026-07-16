aee0e98b88af02a21229ba2c0c6e4e024cf11107
pseudoclone <prince.077bei027@acem.edu.np>
2026-07-16 23:11:24 +0545

    Add ISBN validation

--- book/models.py
+++ book/models.py
@@ -1,6 +1,37 @@
 from django.db import models
 from uuid import uuid4
 from django.core.validators import RegexValidator, MinLengthValidator
+from django.core.exceptions import ValidationError
+
+
+def validate_ISBN(ISBN: str):
+    if len(ISBN) == 13:
+        if not ISBN.isdigit():
+            raise ValidationError("ISBN-13 must only have number")
+        dsum = 0
+        digits = [int(char) for char in ISBN]
+        for idx, digit in enumerate(digits):
+            if (idx) % 2 == 0:
+                dsum += digit
+            else:
+                dsum += digit * 3
+        if dsum % 10 != 0:
+            raise ValidationError("Not a valid 13 digit ISBN")
+
+    elif (len(ISBN)) == 10:
+        dsum = 0
+        for idx, char in enumerate(ISBN):
+            if idx == 9 and char == "X":
+                digit = 10
+            elif char.isdigit():
+                digit = int(char)
+            else:
+                raise ValidationError("Invalid characters in ISBN string")
+            dsum += digit * (10 - idx)
+        if dsum % 11 != 0:
+            raise ValidationError("Not a valid 10 digit ISBN")
+    else:
+        raise ValidationError("ISBN is incorrect")
 
 
 class Book(models.Model):
@@ -10,10 +41,11 @@ class Book(models.Model):
         max_length=13,
         validators=[
             RegexValidator(
-                r"^[0-9]+$",
+                r"^(?:\d{9}[\dX]|\d{13})$",
                 message="Insert only the correct ISBN with numbers",
             ),
             MinLengthValidator(10, message="ISBN is typically 10-13 digits"),
+            validate_ISBN,
         ],
         unique=True,
     )
@@ -21,6 +53,13 @@ class Book(models.Model):
 
     def __str__(self) -> str:
         return f"{self.title}"
+
+    def save(
+        self, *args, **kwargs
+    ):  # Since save won't call the validator, here it will call this on instance
+        # creation of this model
+        self.full_clean()
+        super().save(*args, **kwargs)
 
 
 class BookCopy(models.Model):
