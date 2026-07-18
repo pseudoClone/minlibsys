9879ca9f67906ca6eaa1dd705109281bb515b894
pseudoclone <prince.077bei027@acem.edu.np>
2026-07-18 15:49:41 +0545

    Add url paths for OpenAPI compatible documentation

--- minlibsys/urls.py
+++ minlibsys/urls.py
@@ -2,6 +2,10 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from borrowing.views import BorrowingViewSet
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularSwaggerView,
+)
 
 router = DefaultRouter()
 router.register(r"borrowings", BorrowingViewSet, basename="borrowing")
@@ -10,6 +14,13 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include(router.urls)),
     path(
-        "api-auth/", include("rest_framework.urls", namespace="rest_framework")
+        "api-auth/",
+        include("rest_framework.urls", namespace="rest_framework"),
+    ),
+    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+    path(
+        "api/docs/",
+        SpectacularSwaggerView.as_view(url_name="schema"),
+        name="swagger-ui",
     ),
 ]
