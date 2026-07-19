56d80334968b4294925fdff914e87f54f34ce71e
pseudoclone <prince.077bei027@acem.edu.np>
2026-07-19 12:05:51 +0545

    Create ViewSet and using queryset to show the available books

--- book/views.py
+++ book/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .serializers import BooksViewSerializer
+from rest_framework.permissions import IsAuthenticated
+from .models import Book
 
-# Create your views here.
+
+class BookViewSet(viewsets.ReadOnlyModelViewSet):
+    serializer_class = BooksViewSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return Book.objects.with_availability().prefetch_related("authors")
