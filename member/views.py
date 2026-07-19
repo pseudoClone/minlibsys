091f2510c0b703241c1a25bca88034297d9dcae9
pseudoclone <prince.077bei027@acem.edu.np>
2026-07-19 22:23:21 +0545

    Add APIView using generics for registration

--- member/views.py
+++ member/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.permissions import AllowAny
+from .serializers import MemberSignUpSerializer
+from rest_framework import generics
 
-# Create your views here.
+
+class MemberRegistrationView(generics.CreateAPIView):
+    serializer_class = MemberSignUpSerializer
+    permission_classes = [AllowAny]
